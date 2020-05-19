35a46bb28ca3cdbe68b613d3b3cf39e0672f0c5f
JamieRamone <62776084+JamieRamone@users.noreply.github.com>
2020-05-19 05:31:56 -0300

    Delete Template.m.d

--- Modules/Template/obj/Template.obj/Template.m.d
+++
@@ -1,8 +0,0 @@
-obj/Template.obj/Template.m.o: Template.m ../../aux.h Template.h \
- ../../PreferencePane/PreferencePaneController.h
-
-../../aux.h:
-
-Template.h:
-
-../../PreferencePane/PreferencePaneController.h:

a45fb95631709cd50f06b75bf994583479ad9db7
JamieRamone <62776084+JamieRamone@users.noreply.github.com>
2020-05-19 05:50:15 -0300

    Add files via upload
    Initial commit (UNIX expert module).

---
+++ Modules/UnixExpert/UnixExpert.h
@@ -0,0 +1,41 @@
+/***********************************************************************************************************************************
+ *
+ *	UnixExpert.h
+ *
+ * This file is an part of Mondo Preferences.
+ *
+ *	Copyright (C) 2020 Mondo Megagames.
+ * 	Author: Jamie Ramone <sancombru@gmail.com>
+ *	Date: 18-5-20
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>
+ *
+ **********************************************************************************************************************************/
+#import <Foundation/Foundation.h>
+
+#import "PreferencePaneController.h"
+
+@interface UnixExpertPaneController : PreferencePaneController {
+	id	umaskMatrix;
+	id	unixExpertCheckbox;
+	id	largeFileSystemCheckbox;
+	id	privateWindowServerCheckbox;
+	id	privateSoundServerCheckbox;
+	id	protectedEPSDisplayCheckbox;
+}
+
+- (void) umaskMatrixChangeAction: (id) sender;
+- (void) unixExpertChangeAction: (id) sender;
+- (void) largeFileSystemChangeAction: (id) sender;
+- (void) privateWindowServerChangeAction: (id) sender;
+- (void) privateSoundServerChangeAction: (id) sender;
+- (void) protectedEPSDisplayChangeAction: (id) sender;
+
+@end
